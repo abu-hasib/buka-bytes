2d2bb46281550de619a57235113a186be07e6d73
Ridwan <ricklick262@gmail.com>
2021-05-28 21:05:04 +0100

    base food dash nav

--- src/components/Food/index.js
+++ src/components/Food/index.js
@@ -14,7 +14,21 @@ const Food = () => {
   return (
     // <div className="container mx-auto">
     <div className="grid grid-cols-12 grid-rows-2 h-screen">
-      <div className="bg-red-900 col-start-3 col-end-13 row-start-1 h-12 sticky z-50"></div>
+      <div className="flex justify-around items-center bg-green-300 col-start-3 col-end-13 row-start-1 h-12 sticky z-50">
+        <input
+          className="w-96 h-8 rounded-2xl outline-none p-4 ml-auto"
+          placeholder="Search city"
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 ml-auto"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+        </svg>
+        <img className="rounded-full h-8 w-8 mr-3 ml-3" src={foodavi} />
+      </div>
       <div className="col-start-3 col-end-13 row-start-1 pt-16 overflow-scroll min-h-screen">
         <div className="flex flex-wrap justify-evenly">
           {foodItems.map((item, key) => {
