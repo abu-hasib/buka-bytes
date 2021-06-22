6674852cc968a82cb0670f63eed327fbd0893b58
Ridwan <ricklick262@gmail.com>
2021-06-22 15:21:35 +0100

    angled edge home fix finally

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -182,7 +182,7 @@ export default function Home() {
           </div>
           <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
             <img
-              className="animate-bounceIn h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+              className="poly animate-bounceIn h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
               src={veggies}
               alt=""
             />
